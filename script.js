116c8e17f7d3462ebb9dbee0c8c94359cdf26952
jewel hassan <jewelhassan399@gmail.com>
2023-12-27 01:49:22 +0600

    again change code

--- script.js
+++ script.js
@@ -1,158 +1,83 @@
-// var availableBalance = document.querySelector("#available-balance");
-// var inputAmount = document.querySelector("#input-amount");
-// var transList = document.querySelector("#trans-list");
+var availableBalance = document.querySelector("#available-balance");
+var inputAmount = document.querySelector("#input-amount");
+var transList = document.querySelector("#trans-list");
 
-// var totalAmount = parseInt(availableBalance.innerHTML);
+// Retrieve data from localStorage on page load
+var savedData = localStorage.getItem("transactionData");
+var totalAmount = parseInt(availableBalance.innerHTML);
+if (savedData) {
+    transList.innerHTML = savedData;
+}
 
-// function depoBtn() {
-//     var converted = inputAmount.value;
-//     var converting = parseInt(converted);
-//     var result = totalAmount + converting;
+function depoBtn() {
+    var converted = inputAmount.value;
+    var converting = parseInt(converted);
+    var result = totalAmount + converting;
 
-//     if (converted.trim() !== "") {
-//         updateTransaction("Deposit", converting);
-//         totalAmount = result;
-//         updateAvailableBalance(totalAmount);
-//     }
+    if (converted.trim() !== "") {
+        updateTransaction("Deposit", converting);
+        totalAmount = result;
+        updateAvailableBalance(totalAmount);
+    }
 
-//     inputAmount.value = "";
-// }
+    inputAmount.value = "";
+}
 
-// function expBtn() {
-//     var converted = inputAmount.value;
-//     var converting = parseInt(converted);
-//     var result = totalAmount - converting;
+function expBtn() {
+    var converted = inputAmount.value;
+    var converting = parseInt(converted);
+    var result = totalAmount - converting;
 
-//     if (converted.trim() !== "") {
-//         updateTransaction("Expense", converting);
-//         totalAmount = result;
-//         updateAvailableBalance(totalAmount);
-//     }
+    if (converted.trim() !== "") {
+        updateTransaction("Expense", converting);
+        totalAmount = result;
+        updateAvailableBalance(totalAmount);
+    }
 
-//     inputAmount.value = "";
-// }
+    inputAmount.value = "";
+}
 
-// function updateTransaction(type, amount) {
-//     var listItem = document.createElement("li");
+function updateTransaction(type, amount) {
+    var listItem = document.createElement("li");
 
-//     // Get the current date and time
-//     var now = new Date();
-//     var dateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
-//     var timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    // Add date and time information
+    var currentDate = new Date();
+    var dateTimeString = currentDate.toLocaleString();
 
-//     var dateString = now.toLocaleDateString(undefined, dateOptions);
-//     var timeString = now.toLocaleTimeString(undefined, timeOptions);
+    listItem.textContent = `${type}: ${amount} (${dateTimeString})`;
 
-//     listItem.textContent = `${type}: ${amount} (Date: ${dateString}, Time: ${timeString})`;
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete-btn");
+    deleteButton.addEventListener("click", function () {
+        transList.removeChild(listItem);
+        updateAvailableBalance(totalAmount - amount);
+        saveDataToLocal();
+    });
 
-//     var deleteButton = document.createElement("button");
-//     deleteButton.textContent = "Delete";
-//     deleteButton.classList.add("delete-btn");
-//     deleteButton.addEventListener("click", function () {
-//         transList.removeChild(listItem);
-//         updateAvailableBalance(totalAmount - amount);
-//     });
+    listItem.appendChild(deleteButton);
+    listItem.classList.add(type.toLowerCase());
+    transList.insertBefore(listItem, transList.firstChild);
 
-//     listItem.appendChild(deleteButton);
-//     listItem.classList.add(type.toLowerCase());
-//     transList.insertBefore(listItem, transList.firstChild);
-// }
+    // Save updated data to localStorage
+    saveDataToLocal();
+}
 
-// function updateAvailableBalance(amount) {
-//     availableBalance.innerHTML = amount;
-// }
+function updateAvailableBalance(amount) {
+    availableBalance.innerHTML = amount;
+}
 
-// function resetBtn() {
-//     totalAmount = 0;
-//     availableBalance.innerHTML = totalAmount;
-//     transList.innerHTML = "";
-//     inputAmount.value = "";
-// }
+function resetBtn() {
+    totalAmount = 0;
+    availableBalance.innerHTML = totalAmount;
+    transList.innerHTML = "";
+    inputAmount.value = "";
 
+    // Reset data in localStorage
+    saveDataToLocal();
+}
 
-
-
-        var availableBalance = document.querySelector("#available-balance");
-        var inputAmount = document.querySelector("#input-amount");
-        var transList = document.querySelector("#trans-list");
-
-        // Retrieve data from localStorage on page load
-        var savedData = localStorage.getItem("transactionData");
-        var totalAmount = parseInt(availableBalance.innerHTML);
-        if (savedData) {
-            transList.innerHTML = savedData;
-        }
-
-        function depoBtn() {
-            var converted = inputAmount.value;
-            var converting = parseInt(converted);
-            var result = totalAmount + converting;
-
-            if (converted.trim() !== "") {
-                updateTransaction("Deposit", converting);
-                totalAmount = result;
-                updateAvailableBalance(totalAmount);
-            }
-
-            inputAmount.value = "";
-        }
-
-        function expBtn() {
-            var converted = inputAmount.value;
-            var converting = parseInt(converted);
-            var result = totalAmount - converting;
-
-            if (converted.trim() !== "") {
-                updateTransaction("Expense", converting);
-                totalAmount = result;
-                updateAvailableBalance(totalAmount);
-            }
-
-            inputAmount.value = "";
-        }
-
-        function updateTransaction(type, amount) {
-            var listItem = document.createElement("li");
-
-            // Add date and time information
-            var currentDate = new Date();
-            var dateTimeString = currentDate.toLocaleString();
-
-            listItem.textContent = `${type}: ${amount} (${dateTimeString})`;
-
-            var deleteButton = document.createElement("button");
-            deleteButton.textContent = "Delete";
-            deleteButton.classList.add("delete-btn");
-            deleteButton.addEventListener("click", function () {
-                transList.removeChild(listItem);
-                updateAvailableBalance(totalAmount - amount);
-                saveDataToLocal();
-            });
-
-            listItem.appendChild(deleteButton);
-            listItem.classList.add(type.toLowerCase());
-            transList.insertBefore(listItem, transList.firstChild);
-
-            // Save updated data to localStorage
-            saveDataToLocal();
-        }
-
-        function updateAvailableBalance(amount) {
-            availableBalance.innerHTML = amount;
-        }
-
-        function resetBtn() {
-            totalAmount = 0;
-            availableBalance.innerHTML = totalAmount;
-            transList.innerHTML = "";
-            inputAmount.value = "";
-
-            // Reset data in localStorage
-            saveDataToLocal();
-        }
-
-        function saveDataToLocal() {
-            // Save transaction data to localStorage
-            localStorage.setItem("transactionData", transList.innerHTML);
-        }
-    
+function saveDataToLocal() {
+    // Save transaction data to localStorage
+    localStorage.setItem("transactionData", transList.innerHTML);
+}
