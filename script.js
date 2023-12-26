ce80f2b585a0342327f13d8c51dd15d06d4bd2da
jewel hassan <jewelhassan399@gmail.com>
2023-12-27 01:45:00 +0600

    added permanently data

--- script.js
+++ script.js
@@ -34,15 +34,23 @@
 
 // function updateTransaction(type, amount) {
 //     var listItem = document.createElement("li");
-//     listItem.textContent = `${type}: ${amount}`;
 
+//     // Get the current date and time
+//     var now = new Date();
+//     var dateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
+//     var timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+
+//     var dateString = now.toLocaleDateString(undefined, dateOptions);
+//     var timeString = now.toLocaleTimeString(undefined, timeOptions);
+
+//     listItem.textContent = `${type}: ${amount} (Date: ${dateString}, Time: ${timeString})`;
 
 //     var deleteButton = document.createElement("button");
 //     deleteButton.textContent = "Delete";
 //     deleteButton.classList.add("delete-btn");
 //     deleteButton.addEventListener("click", function () {
 //         transList.removeChild(listItem);
-
+//         updateAvailableBalance(totalAmount - amount);
 //     });
 
 //     listItem.appendChild(deleteButton);
@@ -59,78 +67,92 @@
 //     availableBalance.innerHTML = totalAmount;
 //     transList.innerHTML = "";
 //     inputAmount.value = "";
-
 // }
 
 
 
-var availableBalance = document.querySelector("#available-balance");
-var inputAmount = document.querySelector("#input-amount");
-var transList = document.querySelector("#trans-list");
 
-var totalAmount = parseInt(availableBalance.innerHTML);
+        var availableBalance = document.querySelector("#available-balance");
+        var inputAmount = document.querySelector("#input-amount");
+        var transList = document.querySelector("#trans-list");
 
-function depoBtn() {
-    var converted = inputAmount.value;
-    var converting = parseInt(converted);
-    var result = totalAmount + converting;
+        // Retrieve data from localStorage on page load
+        var savedData = localStorage.getItem("transactionData");
+        var totalAmount = parseInt(availableBalance.innerHTML);
+        if (savedData) {
+            transList.innerHTML = savedData;
+        }
 
-    if (converted.trim() !== "") {
-        updateTransaction("Deposit", converting);
-        totalAmount = result;
-        updateAvailableBalance(totalAmount);
-    }
+        function depoBtn() {
+            var converted = inputAmount.value;
+            var converting = parseInt(converted);
+            var result = totalAmount + converting;
 
-    inputAmount.value = "";
-}
+            if (converted.trim() !== "") {
+                updateTransaction("Deposit", converting);
+                totalAmount = result;
+                updateAvailableBalance(totalAmount);
+            }
 
-function expBtn() {
-    var converted = inputAmount.value;
-    var converting = parseInt(converted);
-    var result = totalAmount - converting;
+            inputAmount.value = "";
+        }
 
-    if (converted.trim() !== "") {
-        updateTransaction("Expense", converting);
-        totalAmount = result;
-        updateAvailableBalance(totalAmount);
-    }
+        function expBtn() {
+            var converted = inputAmount.value;
+            var converting = parseInt(converted);
+            var result = totalAmount - converting;
 
-    inputAmount.value = "";
-}
+            if (converted.trim() !== "") {
+                updateTransaction("Expense", converting);
+                totalAmount = result;
+                updateAvailableBalance(totalAmount);
+            }
 
-function updateTransaction(type, amount) {
-    var listItem = document.createElement("li");
+            inputAmount.value = "";
+        }
 
-    // Get the current date and time
-    var now = new Date();
-    var dateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
-    var timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+        function updateTransaction(type, amount) {
+            var listItem = document.createElement("li");
 
-    var dateString = now.toLocaleDateString(undefined, dateOptions);
-    var timeString = now.toLocaleTimeString(undefined, timeOptions);
+            // Add date and time information
+            var currentDate = new Date();
+            var dateTimeString = currentDate.toLocaleString();
 
-    listItem.textContent = `${type}: ${amount} (Date: ${dateString}, Time: ${timeString})`;
+            listItem.textContent = `${type}: ${amount} (${dateTimeString})`;
 
-    var deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.classList.add("delete-btn");
-    deleteButton.addEventListener("click", function () {
-        transList.removeChild(listItem);
-        updateAvailableBalance(totalAmount - amount);
-    });
+            var deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.classList.add("delete-btn");
+            deleteButton.addEventListener("click", function () {
+                transList.removeChild(listItem);
+                updateAvailableBalance(totalAmount - amount);
+                saveDataToLocal();
+            });
 
-    listItem.appendChild(deleteButton);
-    listItem.classList.add(type.toLowerCase());
-    transList.insertBefore(listItem, transList.firstChild);
-}
+            listItem.appendChild(deleteButton);
+            listItem.classList.add(type.toLowerCase());
+            transList.insertBefore(listItem, transList.firstChild);
 
-function updateAvailableBalance(amount) {
-    availableBalance.innerHTML = amount;
-}
+            // Save updated data to localStorage
+            saveDataToLocal();
+        }
 
-function resetBtn() {
-    totalAmount = 0;
-    availableBalance.innerHTML = totalAmount;
-    transList.innerHTML = "";
-    inputAmount.value = "";
-}
+        function updateAvailableBalance(amount) {
+            availableBalance.innerHTML = amount;
+        }
+
+        function resetBtn() {
+            totalAmount = 0;
+            availableBalance.innerHTML = totalAmount;
+            transList.innerHTML = "";
+            inputAmount.value = "";
+
+            // Reset data in localStorage
+            saveDataToLocal();
+        }
+
+        function saveDataToLocal() {
+            // Save transaction data to localStorage
+            localStorage.setItem("transactionData", transList.innerHTML);
+        }
+    
